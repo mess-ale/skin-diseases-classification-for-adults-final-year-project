c640e5532996d102d4c717133d4ee2407ae8e6a2
meseret alemnew <alemnehmeseret@gmail.com>
2024-04-25 13:23:04 +0300

    renamed

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,12 +1,5 @@
 from django.db import models
-
-
-class User(models.Model):
-    username = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(max_length=100, unique=True)
-    password = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
